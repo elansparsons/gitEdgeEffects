e8b840037e24953b41255d558d754b3690ec8c25
elansparsons <elansparsons@gmail.com>
2018-03-18 06:04:33 -0400

    graphs by biome

--- meta_qualquan.R
+++ meta_qualquan.R
@@ -1,5 +1,6 @@
 library(reshape2)
 library(ggplot2)
+library(gridExtra)
 
 #knit together qualitative & quantitative data
 
@@ -43,6 +44,59 @@ ggplot(tropics,aes(x = just.dist)) +
   geom_line(aes(y=0),color="black")+
   coord_cartesian(xlim=c(-10,250))
 
+ba <- ggplot(tropics,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Temperature and humidity")+
+  geom_smooth(aes(y=percent_diff), color = "lightslateblue",alpha=0) +
+  geom_smooth(aes(y=percentrh_diff), color = "indianred3",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+bb <- ggplot(tropics,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("VPD")+
+  geom_point(aes(y=percentVPD_diff), color = "lightslateblue") +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  coord_cartesian(xlim=c(-10,250))
+
+bc <- ggplot(tropics,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Soil")+
+  geom_smooth(aes(y=percentsm_diff), color = "turquoise3",alpha=0) +
+  geom_smooth(aes(y=percentst_diff), color = "goldenrod2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+bd <- ggplot(tropics,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Light")+
+  geom_smooth(aes(y=percentPAR_diff), color = "maroon2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+be <- ggplot(tropics,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Wind")+
+  geom_smooth(aes(y=percentws_diff), color = "thistle4",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+grid.arrange(ba,bc,bd,be,ncol=2,nrow=2)
+
 #temperate trends
 ggplot(temperate,aes(x = just.dist)) +
   geom_smooth(aes(y=percent_diff), color = "green",alpha=0) +
@@ -59,6 +113,59 @@ ggplot(temperate,aes(x = just.dist)) +
   geom_line(aes(y=0),color="black")+
   coord_cartesian(xlim=c(-10,250))
 
+ca <- ggplot(temperate,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Temperature and humidity")+
+  geom_smooth(aes(y=percent_diff), color = "lightslateblue",alpha=0) +
+  geom_smooth(aes(y=percentrh_diff), color = "indianred3",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+cb <- ggplot(temperate,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("VPD")+
+  geom_point(aes(y=percentVPD_diff), color = "lightslateblue") +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  coord_cartesian(xlim=c(-10,250))
+
+cc <- ggplot(temperate,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Soil")+
+  geom_smooth(aes(y=percentsm_diff), color = "turquoise3",alpha=0) +
+  geom_smooth(aes(y=percentst_diff), color = "goldenrod2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+cd <- ggplot(temperate,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Light")+
+  geom_smooth(aes(y=percentPAR_diff), color = "maroon2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+ce <- ggplot(temperate,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Wind")+
+  geom_smooth(aes(y=percentws_diff), color = "thistle4",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+grid.arrange(ca,cc,cd,ce,ncol=2,nrow=2)
+
 #boreal trends
 
 ggplot(boreal,aes(x = just.dist)) +
@@ -68,6 +175,61 @@ ggplot(boreal,aes(x = just.dist)) +
   geom_point(aes(y=percentsm_diff),color="orange")+
   geom_line(aes(y=0),color="black")+
   coord_cartesian(xlim=c(-10,250),ylim=c(-30,30))
+
+da <- ggplot(boreal,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Temperature and humidity")+
+  geom_smooth(aes(y=percent_diff), color = "lightslateblue",alpha=0) +
+  geom_smooth(aes(y=percentrh_diff), color = "indianred3",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+db <- ggplot(boreal,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("VPD")+
+  geom_point(aes(y=percentVPD_diff), color = "lightslateblue") +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  coord_cartesian(xlim=c(-10,250))
+
+dc <- ggplot(boreal,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Soil")+
+  geom_smooth(aes(y=percentsm_diff), color = "turquoise3",alpha=0) +
+  geom_smooth(aes(y=percentst_diff), color = "goldenrod2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+dd <- ggplot(boreal,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Light")+
+  geom_smooth(aes(y=percentPAR_diff), color = "maroon2",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+de <- ggplot(boreal,aes(x = just.dist)) +
+  theme_classic()+
+  ggtitle("Wind")+
+  geom_smooth(aes(y=percentws_diff), color = "thistle4",alpha=0) +
+  xlab("Distance from edge")+
+  ylab("% difference from interior point")+
+  geom_line(aes(y=0),color="black")+
+  geom_vline(xintercept=0) +
+  coord_cartesian(xlim=c(-10,250))
+
+#no data for light and wind
+
+grid.arrange(da,dc,dd,de,ncol=2,nrow=2)
 
 #temperate to compare to boreal
 ggplot(temperate,aes(x = just.dist)) +
@@ -94,26 +256,33 @@ intermed <- log1p((quanonly[,c(6,16,19,22,25,28,31)]/100))
 names(intermed) <- c("proat_diff","propar_diff","prorh_diff","prosm_diff","prost_diff","provpd_diff","prows_diff")
 forlatglm <- cbind(intermed,quanonly)
 
-atlat <- glm(proat_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
+atlat <- glm(proat_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm) #better
 atlat2 <- glm(proat_diff ~ simple.lat, family = gaussian, data = forlatglm)
+atlat3 <- glm(proat_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #better
 
-rhlat <- glm(prorh_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
+rhlat <- glm(prorh_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm) #better
 rhlat2 <- glm(prorh_diff ~ simple.lat, family = gaussian, data = forlatglm)
+rhlat3 <- glm(prorh_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #better
 
 vpdlat <- glm(provpd_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
 vpdlat2 <- glm(provpd_diff ~ simple.lat, family = gaussian, data = forlatglm)
+vpdlat3 <- glm(provpd_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #best
 
-stlat <- glm(prost_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
+stlat <- glm(prost_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm) #better
 stlat2 <- glm(prost_diff ~ simple.lat, family = gaussian, data = forlatglm)
+stlat3 <- glm(prost_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #better
 
 smlat <- glm(prosm_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
-smlat2 <- glm(prosm_diff ~ simple.lat, family = gaussian, data = forlatglm)
+smlat2 <- glm(prosm_diff ~ simple.lat, family = gaussian, data = forlatglm) #somewhat better
+smlat3 <- glm(prosm_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm)
 
-wslat <- glm(prows_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
+wslat <- glm(prows_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm) #somewhat better
 wslat2 <- glm(prows_diff ~ simple.lat, family = gaussian, data = forlatglm)
+wslat3 <- glm(prows_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #somewhat better
 
-parlat <- glm(propar_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm)
+parlat <- glm(propar_diff ~ just.dist + simple.lat, family = gaussian, data = forlatglm) #better
 parlat2 <- glm(propar_diff ~ simple.lat, family = gaussian, data = forlatglm)
+parlat3 <- glm(propar_diff ~ just.dist * simple.lat, family = gaussian, data = forlatglm) #better
 
 summary(lm(proat_diff ~ simple.lat,data=forlatglm))
 
